d1aa97d5558cd4b18aa0d16d46f4872468f0c261
kim_seyeon <rlatpdus852@gmail.com>
2023-10-31 16:17:45 +0900

    세연_profileDTO

--- src/main/java/com/one/mat/member/dto/ProfileDTO.java
+++ src/main/java/com/one/mat/member/dto/ProfileDTO.java
@@ -1,94 +1,105 @@
 package com.one.mat.member.dto;
 
 public class ProfileDTO {
-public int member_idx;
-public String pro_dogName;
-public int pro_dogAge;
-public String pro_dogGender;
-public String pro_dogDesc;
-public String pro_rep;
-public int pro_dogScore;
-public String breedType;
-public String charType;
-public int breedType_code;
-public int charType_code;
 
-
-
-public int getCharType_code() {
-	return charType_code;
-}
-public void setCharType_code(int charType_code) {
-	this.charType_code = charType_code;
-}
-public int getBreedType_code() {
-	return breedType_code;
-}
-public void setBreedType_code(int breedType_code) {
-	this.breedType_code = breedType_code;
-}
-public String photos;
-
-
-public String getPhotos() {
-	return photos;
-}
-public void setPhotos(String photos) {
-	this.photos = photos;
-}
-public String getCharType() {
-	return charType;
-}
-public void setCharType(String charType) {
-	this.charType = charType;
-}
-public String getBreedType() {
-	return breedType;
-}
-public void setBreedType(String breedType) {
-	this.breedType = breedType;
-}
-public int getMember_idx() {
-	return member_idx;
-}
-public void setMember_idx(int member_idx) {
-	this.member_idx = member_idx;
-}
-public String getPro_dogName() {
-	return pro_dogName;
-}
-public void setPro_dogName(String pro_dogName) {
-	this.pro_dogName = pro_dogName;
-}
-public int getPro_dogAge() {
-	return pro_dogAge;
-}
-public void setPro_dogAge(int pro_dogAge) {
-	this.pro_dogAge = pro_dogAge;
-}
-public String getPro_dogGender() {
-	return pro_dogGender;
-}
-public void setPro_dogGender(String pro_dogGender) {
-	this.pro_dogGender = pro_dogGender;
-}
-public String getPro_dogDesc() {
-	return pro_dogDesc;
-}
-public void setPro_dogDesc(String pro_dogDesc) {
-	this.pro_dogDesc = pro_dogDesc;
-}
-public String getPro_rep() {
-	return pro_rep;
-}
-public void setPro_rep(String pro_rep) {
-	this.pro_rep = pro_rep;
-}
-public int getPro_dogScore() {
-	return pro_dogScore;
-}
-public void setPro_dogScore(int pro_dogScore) {
-	this.pro_dogScore = pro_dogScore;
-}
-
+	private int pro_idx;
+	private int member_idx;
+	private String pro_dogName;
+	private int pro_dogAge;
+	private String pro_dogGender;
+	private String pro_dogDesc;
+	private int pro_dogScore;
+	private String pro_rep; // 대표프로필 여부
+	private String photo_fileName; // 프로필에 들어갈 사진
+	private String breedType;
+	private String charType;
+	private String photos;
+	private String breedType_code;
+	private String charType_code;
+	
+	public String getBreedType_code() {
+		return breedType_code;
+	}
+	public void setBreedType_code(String breedType_code) {
+		this.breedType_code = breedType_code;
+	}
+	public String getCharType_code() {
+		return charType_code;
+	}
+	public void setCharType_code(String charType_code) {
+		this.charType_code = charType_code;
+	}
+	public String getPhoto_fileName() {
+		return photo_fileName;
+	}
+	public void setPhoto_fileName(String photo_fileName) {
+		this.photo_fileName = photo_fileName;
+	}
+	public String getBreedType() {
+		return breedType;
+	}
+	public void setBreedType(String breedType) {
+		this.breedType = breedType;
+	}
+	public String getCharType() {
+		return charType;
+	}
+	public void setCharType(String charType) {
+		this.charType = charType;
+	}
+	public String getPhotos() {
+		return photos;
+	}
+	public void setPhotos(String photos) {
+		this.photos = photos;
+	}
+	public int getPro_idx() {
+		return pro_idx;
+	}
+	public void setPro_idx(int pro_idx) {
+		this.pro_idx = pro_idx;
+	}
+	public int getMember_idx() {
+		return member_idx;
+	}
+	public void setMember_idx(int member_idx) {
+		this.member_idx = member_idx;
+	}
+	public String getPro_dogName() {
+		return pro_dogName;
+	}
+	public void setPro_dogName(String pro_dogName) {
+		this.pro_dogName = pro_dogName;
+	}
+	public int getPro_dogAge() {
+		return pro_dogAge;
+	}
+	public void setPro_dogAge(int pro_dogAge) {
+		this.pro_dogAge = pro_dogAge;
+	}
+	public String getPro_dogGender() {
+		return pro_dogGender;
+	}
+	public void setPro_dogGender(String pro_dogGender) {
+		this.pro_dogGender = pro_dogGender;
+	}
+	public String getPro_dogDesc() {
+		return pro_dogDesc;
+	}
+	public void setPro_dogDesc(String pro_dogDesc) {
+		this.pro_dogDesc = pro_dogDesc;
+	}
+	public int getPro_dogScore() {
+		return pro_dogScore;
+	}
+	public void setPro_dogScore(int pro_dogScore) {
+		this.pro_dogScore = pro_dogScore;
+	}
+	public String getPro_rep() {
+		return pro_rep;
+	}
+	public void setPro_rep(String pro_rep) {
+		this.pro_rep = pro_rep;
+	}
+	
 }
