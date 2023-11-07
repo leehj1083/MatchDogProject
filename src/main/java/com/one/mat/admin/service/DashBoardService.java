a4499bd514a6bbdfe9305276a880d6526edc9fed
lee_han_jun <leehj1083@naver.com>
2023-11-07 15:19:08 +0900

    대시보드서비스

---
+++ src/main/java/com/one/mat/admin/service/DashBoardService.java
@@ -0,0 +1,16 @@
+package com.one.mat.admin.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.one.mat.admin.dao.DashBoardDAO;
+
+@Service
+public class DashBoardService {
+
+	Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired DashBoardDAO dao;
+	
+}
